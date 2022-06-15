cae9e6e303321a0f11a1d0833efd453294d894f3
yu teng hsueh <jon111rock@gmail.com>
2022-06-16 03:25:30 +0800

    style: 增加pagination選取樣式

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 const Pagination = (props: Props) => {
+  const [isActive, setIsActive] = useState<string>("所有訂單");
+
   return (
     <ul className=" flex gap-5 mb-2 text-lightGray ">
-      <li className="p-1 cursor-pointer transition-all duration-300 hover:text-blueGrean">
+      <li
+        className={`pagination ${isActive === "所有訂單" ? "active" : ""}`}
+        onClick={() => {
+          setIsActive("所有訂單");
+        }}
+      >
+        所有訂單 <span className="p-1 bg-gray rounded-lg">80</span>
+      </li>
+      <li
+        className={`pagination ${isActive === "準備中" ? "active" : ""}`}
+        onClick={() => {
+          setIsActive("準備中");
+        }}
+      >
         準備中 <span className="p-1 bg-gray rounded-lg">80</span>
       </li>
-      <li className="p-1 cursor-pointer transition-all duration-300 hover:text-blueGrean">
+      <li
+        className={`pagination ${isActive === "已出貨" ? "active" : ""}`}
+        onClick={() => {
+          setIsActive("已出貨");
+        }}
+      >
         已出貨 <span className="p-1 bg-gray rounded-lg">70</span>
       </li>
-      <li className="p-1 cursor-pointer transition-all duration-300 hover:text-blueGrean">
+      <li
+        className={`pagination ${isActive === "已完成" ? "active" : ""}`}
+        onClick={() => {
+          setIsActive("已完成");
+        }}
+      >
         已完成 <span className="p-1 bg-gray rounded-lg">50</span>
       </li>
     </ul>
