46aff031df95a4415d5d242bbbd4cc54092fc21c
yu teng hsueh <jon111rock@gmail.com>
2022-06-18 15:32:04 +0800

    feat: 新增商品至資料庫

--- src/Pages/ItemTable/ItemTable.tsx
+++ src/Pages/ItemTable/ItemTable.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createItem } from "../../api/itemApi";
 
 type Props = {};
 
 const ItemTable = (props: Props) => {
   const navigate = useNavigate();
+  const [name, setName] = useState<string>("");
+  const [price, setPrice] = useState<number>(0);
+
+  const handleSave = async () => {
+    if (!name || !price) return;
+    const tempItem = { name: name, price: price };
+    await createItem(tempItem);
+    navigate(-1);
+  };
 
   const handleCancel = () => {
     navigate(-1);
@@ -15,14 +25,33 @@ const ItemTable = (props: Props) => {
       <div className="p-10 bg-white flex flex-col gap-5 rounded-xl">
         <div>
           <span>Name</span>
-          <input className="order-table-input" type="text" />
+          <input
+            className="order-table-input"
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
         <div>
           <span>Price</span>
-          <input className="order-table-input" type="text" />
+          <input
+            className="order-table-input"
+            type="text"
+            onChange={(e) => {
+              setPrice(Number(e.target.value));
+            }}
+          />
         </div>
         <div className="flex justify-end">
-          <button className="button-blue">儲存</button>
+          <button
+            className="button-blue"
+            onClick={() => {
+              handleSave();
+            }}
+          >
+            儲存
+          </button>
           <button
             className="button-border-blue"
             onClick={() => {
--- src/api/itemApi.tsx
+++ src/api/itemApi.tsx
@@ -1,15 +1,23 @@
 import axios from "axios";
 import Item from "../types/Item";
 
+const baseUrl = "https://rifu-order-manager-api.herokuapp.com/api/item";
+
 const getItems = async () => {
   try {
-    const res = await axios.get(
-      "https://rifu-order-manager-api.herokuapp.com/api/item"
-    );
+    const res = await axios.get(baseUrl);
     return res.data.result as Item[];
   } catch (error) {
     return null;
   }
 };
 
-export { getItems };
+const createItem = async (item: { name: string; price: number }) => {
+  try {
+    await axios.post(baseUrl, item);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getItems, createItem };
